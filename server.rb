3a6a2701693ecbd5245d89e3f011d1eb06b1af39
DALM1 <dimitri.almon@gmail.com>
2024-04-11 19:40:05 +0200

    Updates

--- server.rb
+++ server.rb
@@ -1,31 +1,30 @@
 require 'socket'
 
-# Définir le mot de passe pour se connecter au serveur
+
 PASSWORD = "123123123"
 
 server = TCPServer.new(3000)
 
-puts "Lightning fast at 3000 port"
+puts "Server lightning fast at port 3000"
 
 loop do
-  client = server.accept    # Accepter une nouvelle connexion du client
+  client = server.accept   
 
-  # Demander le mot de passe à l'utilisateur
+
   client.puts "ENTER THE PASSWORD :"
   password_attempt = client.gets.chomp
 
   if password_attempt == PASSWORD
-    client.puts "CORRECT PASSWORD, ENTER UR USERNAME :"
+    client.puts "Alohomora ¨¨ ENTER A USERNAME :"
     username = client.gets.chomp
-    client.puts "Welcome Wiazrd #{username} "
 
-    # Boucle pour gérer les messages entrants de l'utilisateur
+
     loop do
       message = client.gets.chomp
-      puts "#{username}> #{message}"
+      puts "#{username}: #{message}"
     end
   else
-    client.puts "YOU ARE NOT ALLOWED HERE, BYE"
+    client.puts "WRONG, UR NOT ALLOWED HERE, BYE"
     client.close
   end
 end
